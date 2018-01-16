363117593ddf77670eed9dfdda9db043975ad148
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-01-16 11:37:47 -0500

    ...

--- Harvest/src/mainMenu/Characters.java
+++ Harvest/src/mainMenu/Characters.java
@@ -1,9 +1,14 @@
 package mainMenu;
 
-public class Characters {
+public class Characters extends MenuScreenLubna{
+
+	public Characters(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
 	}
 
---
+++ Harvest/src/mainMenu/SaveDataJessi.java
@@ -0,0 +1,9 @@
+package mainMenu;
+
+public class SaveDataJessi implements java.io.Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public String name;
+    public int hp;
+}
