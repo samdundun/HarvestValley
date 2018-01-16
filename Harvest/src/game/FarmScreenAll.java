3ec27e013a74de4ef7fea08eb2abaa437c4762e5
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2018-01-16 11:19:16 -0500

    asdsd

--- Harvest/src/game/FarmScreenAll.java
+++ Harvest/src/game/FarmScreenAll.java
@@ -10,7 +10,6 @@ import guiTeacher.userInterfaces.FullFunctionScreen;
 public class FarmScreenAll extends FullFunctionScreen {
 	
 	private Graphic back;
-	private ArrayList<CropJane> crops;
 
 	public FarmScreenAll(int width, int height) {
 		super(width, height);
