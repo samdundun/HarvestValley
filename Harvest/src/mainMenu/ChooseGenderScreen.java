c37edbbd07cda92d182a3ac972d08fda63e89948
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2018-01-16 11:43:19 -0500

    pushh

--- Harvest/src/mainMenu/ChooseGenderScreen.java
+++ Harvest/src/mainMenu/ChooseGenderScreen.java
@@ -8,18 +8,35 @@ import guiTeacher.userInterfaces.ClickableScreen;
 
 public class ChooseGenderScreen extends ClickableScreen implements Runnable {
 
+//	CustomImageButton
+	private Graphic girl;
+	private Graphic boy;
+	private Graphic back;
 
 	public ChooseGenderScreen(int width, int height) {
 		super(width, height);
 		Thread app = new Thread(this);
 		app.start();
 	}
+	
+//	public void resize(int w, int h){
+//		back.loadImages("resources/background.png", w, h);
+//		back.update();
+//		girl.setX(w/2-girl.getWidth());
+//		girl.setY(h/2-girl.getHeight());
+//		boy.setX(w/2+boy.getWidth());
+//		boy.setY(h/2+boy.getHeight());
+//		super.resize(w, h);
+//	}
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		Graphic back = new Graphic(0,0,1,"resources/background.png");
+		back = new Graphic(0,0,getWidth(),getHeight,"resources/background.png");
 		viewObjects.add(back);
-
+		girl = new Graphic(350,350, 150,150,"resources/girlButton.png");
+		viewObjects.add(girl);
+		boy = new Graphic(550,350, 150,150,"resources/boyButton.png");
+		viewObjects.add(boy);
 	}
 
 	@Override
--- Harvest/src/mainMenu/MainMenu.java
+++ Harvest/src/mainMenu/MainMenu.java
@@ -12,11 +12,14 @@ public class MainMenu extends GUIApplication {
 	@Override
 	public void initScreen() {
 		MenuScreenLubna screen = new MenuScreenLubna(getWidth(), getHeight());
-		setScreen(screen);
+		ChooseGenderScreen screen2 = new ChooseGenderScreen(getWidth(), getHeight());
+		setScreen(screen2);
 	}
+	
+	
 
 	public static void main(String[] args) {
-		MainMenu s = new MainMenu(800,600);
+		MainMenu s = new MainMenu(870,550);
 		Thread runner = new Thread(s);
 		runner.start();
 	}
