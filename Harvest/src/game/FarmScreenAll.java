9ff5ac54dd7a3c1fade605e0c8e42d3ceb4dc919
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2018-01-17 11:20:35 -0500

    erte

--- Harvest/src/game/FarmScreenAll.java
+++ Harvest/src/game/FarmScreenAll.java
@@ -23,34 +23,16 @@ public class FarmScreenAll extends FullFunctionScreen {
 	public void initAllObjects(List<Visible> viewObjects) {
 		back = new Graphic(0, 0, "resources/farm.PNG");
 		viewObjects.add(back);
-
-<<<<<<< HEAD
-		menu = new Button(5, 506, 80, 30, "Menu", new Color(230, 235, 210), null);
-		viewObjects.add(menu);
-=======
+		
 		menuJenny = new Button(5, 470, 80, 30, "Menu", new Color(230, 235, 210), null);
 		viewObjects.add(menuJenny);
->>>>>>> branch 'farm' of https://github.com/samdundun/HarvestValley.git
-		
-<<<<<<< HEAD
-		shopJenny = new Button(90, 506, 80, 30, "Shop",new Color(230, 235, 210), null);
-=======
+
 		shopJenny = new Button(90, 470, 80, 30, "Shop", new Color(230, 235, 210), null);
->>>>>>> branch 'farm' of https://github.com/samdundun/HarvestValley.git
 		viewObjects.add(shopJenny);
 		
-<<<<<<< HEAD
-		itemJane = new Button(175, 506, 80, 30, "Inventory", new Color(230, 235, 210), null);
-		viewObjects.add(itemJane);
-		
-		addfarmingPatch(viewObjects);
-		
-		
-
-=======
 		itemJane = new Button(175, 470, 80, 30, "Inventory", new Color(230, 235, 210), null);
 		viewObjects.add(itemJane);
->>>>>>> branch 'farm' of https://github.com/samdundun/HarvestValley.git
+
 	}
 
 	private void addfarmingPatch(List<Visible> viewObjects) {
