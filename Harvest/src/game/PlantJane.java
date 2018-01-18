18e4efc18c9bc7845d9e860284d01677251b77ef
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2018-01-18 11:13:05 -0500

    sdfs

--- Harvest/src/game/PlantJane.java
+++ Harvest/src/game/PlantJane.java
@@ -1,6 +1,13 @@
 package game;
 
+import java.util.ArrayList;
+
 public class PlantJane {
+	private int plantLimit;
+	private String name;
+	private int growthRate;
+	private ArrayList<String> images;
+	private plantProduceJane produce;
 	
 	public PlantJane() {
 		// TODO Auto-generated constructor stub
---
+++ Harvest/src/game/plantProduceJane.java
@@ -0,0 +1,9 @@
+package game;
+
+public class plantProduceJane {
+
+	public plantProduceJane() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
