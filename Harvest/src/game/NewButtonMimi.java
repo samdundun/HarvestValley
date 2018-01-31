bb76690f92a65250264e49ddfe28737b3462e582
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2018-01-31 10:51:48 -0500

    lubna is forcing me 2 do dis

--- Harvest/src/game/NewButtonMimi.java
+++ Harvest/src/game/NewButtonMimi.java
@@ -6,7 +6,24 @@ public class NewButtonMimi extends Button{
 
 	public NewButtonMimi(int x, int y, int w, int h, String text, Action action) {
 		super(x, y, w, h, text, action);
-		// TODO Auto-generated constructor stub
 	}
-
+	
+	//choose gender screen goes first (before starting a thread for new game)
+	
+	public static void main(String[] args) {
+		NewButtonMimi n = new NewButtonMimi(380,350, 150,150, "new", new Action() {
+			
+			@Override
+			public void act() {
+				
+			}
+		});
+		Thread runner = new Thread(n);
+		runner.start();
+	}
+	
+	public void initScreen() {
+		FarmScreenAll f = new FarmScreenAll(getWidth(), getHeight());
+		setScreen(f);
+	}
 }
