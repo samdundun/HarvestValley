d9710583cd47b736ea157847419a39c4f9e7707f
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2018-01-31 11:37:10 -0500

    NEED TO WORK ON OUR CHARACTERS

---
+++ Harvest/src/game/mainScreen/AnimationCharacter.java
@@ -0,0 +1,9 @@
+package game.mainScreen;
+
+public class AnimationCharacter {
+
+	public AnimationCharacter() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Harvest/src/game/mainScreen/Character.java
@@ -0,0 +1,10 @@
+package game.mainScreen;
+
+public class Character {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
