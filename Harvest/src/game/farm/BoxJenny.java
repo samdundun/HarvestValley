136e7f899021a85dbc6c729e36e23764dd2be505
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2018-02-01 11:20:55 -0500

    scale image to size of the animals

--- Harvest/src/game/farm/BoxJenny.java
+++ Harvest/src/game/farm/BoxJenny.java
@@ -1,11 +1,26 @@
 package game.farm;
 
-import game.mainScreen.ImageButton;
-import guiTeacher.components.Action;
+import java.awt.Color;
+import java.awt.Graphics2D;
 
-public class BoxJenny extends ImageButton {
+import guiTeacher.components.Action;
+import guiTeacher.components.CustomImageButton;
+import guiTeacher.components.Graphic;
+import guiTeacher.interfaces.DrawInstructions;
+
+public class BoxJenny extends CustomImageButton {
 
 	public BoxJenny(int x, int y, int w, int h, String imageAddress, Action action) {
-		super(x, y, w, h, imageAddress, action);
+		super(x, y, w, h, new DrawInstructions() {
+			Graphic image = new Graphic(0,0,imageAddress);
+
+			public void draw(Graphics2D g, boolean highlight) {
+				g.drawImage(image.getImage(), 0, 0, null);
+				if(highlight) {
+					g.setColor(new Color(0,0,0,20));
+					g.fillRect(0, 0, image.getWidth(), image.getHeight());
+				}
+			}
+		}, action);
 	}
 }
