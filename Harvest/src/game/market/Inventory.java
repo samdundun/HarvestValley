354cb30c2d3e1b4b5c8d5811bb3cdb35b3288127
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2018-02-02 11:33:11 -0500

    fasdf

--- Harvest/src/game/market/Inventory.java
+++ Harvest/src/game/market/Inventory.java
@@ -29,7 +29,7 @@ public class Inventory {
 	
 	//image index
 	//cornseed,pepperseed,potatoseed,strawberryseed,tomatoseed,wheatseed,corn,pepper,potato,strawberry,tomato,wheat
-	// 0           1          2            3             4          5      6    7       8       9         10    11
+	// 0           1          2            3             4          5      6    7       8        9         10    11
 	public Inventory() {
 		inventItems = new ArrayList<Item>();
 		invent = new ArrayList<Item>();
