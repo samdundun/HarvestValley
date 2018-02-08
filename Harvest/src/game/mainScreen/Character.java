76c63d9c6d425093a70dcef7c1c43fe85bb9e457
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2018-02-08 10:50:42 -0500

    hello is it me you're looking for

--- Harvest/src/game/mainScreen/Character.java
+++ Harvest/src/game/mainScreen/Character.java
@@ -4,6 +4,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import guiTeacher.components.AnimatedComponent;
+import harvest.MainMenu;
 
 public class Character implements KeyListener {
 	
@@ -11,34 +12,50 @@ public class Character implements KeyListener {
 	  *@author Mimi Buan and Lubna Khalid and Jessi Wu
 	 */
 	
-	private static AnimatedComponent goil;
-	private static AnimatedComponent boi;
+	private static AnimatedComponent girl;
+	private static AnimatedComponent boy;
 	private int x;
-	private int y;
-	
+	private int y; 
 	public Character() {
-		boi = game.farm.FarmScreenAll.boy;
-		goil = game.farm.FarmScreenAll.girl;
-		x = 430;
-		y = 200;
+		boy = game.farm.FarmScreenAll.boy;
+		girl = game.farm.FarmScreenAll.girl;
+//		x = 430;
+//		y = 200;
+		this.x = x;
+		this.y = y;
 	}
 	
+	public void updateCharacter() {
+		if(MainMenu.isGirl == true) {
+			girl.update();
+		}
+		else {
+			boy.update();
+		}
+	}
 	public void keyPressed(KeyEvent e) {
 		x = getX();
 		y = getY(); 
-		if (e.getKeyCode() == KeyEvent.VK_LEFT)
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
 			x -= 10;
-			goil.update();
-	    else if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+			updateCharacter();
+		}
+	    else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 	    	x += 10;
-			
-	    else if (e.getKeyCode() == KeyEvent.VK_UP)
+	    	updateCharacter();
+	    }
+	    else if (e.getKeyCode() == KeyEvent.VK_UP) {
 	        y -= 10;
-	    else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+	        updateCharacter();
+		}
+	    else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 	        y += 10;
+	        updateCharacter();
+	    }
 	
 	}
 
+	
 	
 	private int getY() {
 		// TODO Auto-generated method stub
